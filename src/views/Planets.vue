a54d88697a1f5b8ad3b4e804af3417ad312672c3
Joe S <honestcomrade@gmail.com>
2018-09-06 20:14:48 -0700

    loader

--- src/views/Planets.vue
+++ src/views/Planets.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <ul id="example-1">
+    <div v-if="loading" class="loader-image"></div>
+    <ul v-if="!loading">
       <li v-for="planet in results" :key="planet.name">
         {{ planet.name }}
       </li>
@@ -12,6 +12,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { DataFetch } from '@/services/fetch-service.ts';
+import { setTimeout } from 'timers';
 const getter = new DataFetch();
 export default Vue.extend ({
   data() {
@@ -22,8 +23,10 @@ export default Vue.extend ({
   },
   created() {
     getter.getData('planets').then((response) => {
-      this.loading = false;
       this.results = response.results;
+      setTimeout(() => {
+        this.loading = false;
+      }, 2500);
       console.log(response);
     }).catch((err) => {
       console.error(err);
@@ -33,6 +36,20 @@ export default Vue.extend ({
 </script>
 <style lang="scss">
   .home {
+    height: calc(100% - 83px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    .loader-image {
+      border-radius: 10px;
+      box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+      height: 400px;
+      width: 350px;
+      background-image: url(../assets/tumblr_ncongzaETr1s2wio8o1_500.gif);
+      background-position: center;
+      background-size: cover;
+    }
     ul {
       list-style-type: none;
       list-style: none;
